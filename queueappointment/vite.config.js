e054cec08eff0926980aef31dcf3622b217b5154
sumayyasalahudeen <sumayyasalah123@gmail.com>
2026-05-28 03:05:13 +0530

    Fix Netlify deployment config

--- queueappointment/vite.config.js
+++ queueappointment/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/queue-appointment-system/',
+  base: process.env.NETLIFY ? '/' : '/queue-appointment-system/',
   plugins: [react()],
 })
